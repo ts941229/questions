0a3de11c6da5466a2c9dcc9a1b760b0d2bbe5752
taeseok <tsk941229@gmail.com>
2023-08-12 09:41:53 +0900

    first commit

---
+++ src/programmers/Level0.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Level0 {
+	public static void main(String[] args) {
+	
+	}
+	
+	// 각도기 문제
+	public static int protractor(int angle) {
+        int answer = 0;
+        return answer;
+    }
+	
+}
