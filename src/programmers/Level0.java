59aa232de8d094249044d7fa594924c81eeaea8f
taeseok <tsk941229@gmail.com>
2023-08-26 11:05:51 +0900

    few questions

--- src/programmers/Level0.java
+++ src/programmers/Level0.java
@@ -1,9 +1,13 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Level0 {
 	public static void main(String[] args) {
 		
-		completion_condtion_of_triangle(new int[] {199,72,222});
+		System.out.println(remove_vowels("nice to meet you"));
 		
 	}
 	
@@ -197,6 +201,141 @@ public class Level0 {
         return answer;
     }
     
+    // 편지
+    public static int letter(String message) {
+        int answer = 0;
+        
+        answer = message.length() * 2;
+        
+        return answer;
+    }
+    
+    // 문자열 뒤집기
+    public static String turn_string_over(String my_string) {
+        String answer = "";
+        char[] answer_array = new char[my_string.toCharArray().length];
+        int idx = 0;
+        for(int i=my_string.toCharArray().length-1; i>=0; i--) {
+        	answer_array[idx] = my_string.toCharArray()[i];
+        	idx++;
+        }
+        answer = String.valueOf(answer_array);
+        return answer;
+    }
 	
+    // 짝수와 홀수 갯수
+    public static int[] num_even_odd(int[] num_list) {
+        int[] answer = new int[2];
+        int even = 0;
+        int odd = 0;
+        for(int i=0; i<num_list.length; i++) {
+        	if(num_list[i]%2==0) {
+        		even+=1;
+        	}else {
+        		odd+=1;
+        	}
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        
+        return answer;
+    }
+    
+    // 머쓱이보다 키큰사람
+    public static int taller_than_mussk(int[] array, int height) {
+        int answer = 0;
+        
+        for(int i=0; i<array.length; i++) {
+        	if(array[i]>height) {
+        		answer++;
+        	}
+        }
+        
+        return answer;
+    }
+    
+    // 최댓값 만들기
+    public static int make_maximum(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        if(numbers.length>1) {
+        	answer = numbers[numbers.length-1]*numbers[numbers.length-2];
+        }
+
+        return answer;
+    }
+    
+    // 점의 위치 구하기
+    public static int position_of_point(int[] dot) {
+        int answer = 0;
+        
+        if(dot[0]>0 && dot[1]>0) {
+        	answer = 1;
+        }else if(dot[0]<0 && dot[1]>0) {
+        	answer = 2;
+        }else if(dot[0]<0 && dot[1]<0) {
+        	answer = 3;
+        }else if(dot[0]>0 && dot[1]<0) {
+        	answer = 4;
+        }
+        
+        return answer;
+    }
+    
+    // 문자열안에 문자열
+    public static int string_in_string(String str1, String str2) {
+        int answer = 0;
+        
+        if(str1.indexOf(str2)>=0) {
+        	answer = 1;
+        }else {
+        	answer = 2;
+        }
+        
+        return answer;
+    }
+    
+    // 중복된 숫자 개수
+    public static int number_of_duplicate_numbers(int[] array, int n) {
+        int answer = 0;
+        
+        for(int i=0; i<array.length; i++) {
+        	
+        	if(array[i]==n) {
+        		answer++;
+        	}
+        	
+        }
+        
+        return answer;
+    }
+    
+    // 모음제거
+    public static String remove_vowels(String my_string) {
+        String answer = "";
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        StringBuilder sb = new StringBuilder(my_string);
+        ArrayList<Integer> idx_list = new ArrayList<Integer>();
+        for(int i=0; i<my_string.length(); i++) {
+        	for(int j=0; j<vowels.length; j++) {
+        		if(vowels[j].equals(String.valueOf(my_string.charAt(i)))) {
+        			idx_list.add(i);
+        		}
+        	}
+        }
+        
+        idx_list.sort(Collections.reverseOrder());
+
+        for(int i=0; i<idx_list.size(); i++) {
+        	sb.deleteCharAt(idx_list.get(i));
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+    
 	
 }
